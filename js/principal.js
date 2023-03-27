6c404049469031b4196996a5df78e2f4630514c5
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-27 14:51:03 -0300

    implementando marcação visual para indicar as linhas da tabela  que possuem valores inválidos

--- js/principal.js
+++ js/principal.js
@@ -43,19 +43,22 @@ for (let i = 0; i < pacientes.length; i++) {
 
   if (peso <= 0 || peso >= 1000) {
     pesoValido = false;
-    tdImc.textContent = "peso inválido";
-    console.log("peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    console.log("Peso inválido");
+    pacientes[i].classList.add("paciente-invalido");
   }
 
   if (altura <= 0 || altura >= 3.0) {
     alturaValida = false;
-    tdImc.textContent = "altura inválida";
-    console.log("altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    console.log("Altura inválida!");
+    pacientes[i].classList.add("paciente-invalido");
   }
 
   if (pesoValido == false && alturaValida == false) {
-    tdImc.textContent = "altura e peso não válidos";
-    console.log("altura e peso não válidos");
+    tdImc.textContent = "Altura e peso inválidos!";
+    console.log("Altura e peso inválidos");
+    pacientes[i].classList.add("paciente-invalido");
   }
 
   if (alturaValida && pesoValido) {
