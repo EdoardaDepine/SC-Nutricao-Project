7417b3644d6fd8ef81770121b5cdf0bff906d3a0
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-28 17:07:31 -0300

    filtrando pacientes por meio de Eventos

---
+++ js/filtrar.js
@@ -0,0 +1,25 @@
+const campoFiltrar = document.querySelector("#filtrar-tabela");
+
+campoFiltrar.addEventListener("input", function () {
+  const pacientes = document.querySelectorAll(".paciente");
+
+  if (this.value.length > 0) {
+    for (let i = 0; 0 < pacientes.length; i++) {
+      const paciente = pacientes[i];
+      const tdNome = paciente.querySelector(".info-nome");
+      const nome = tdNome.textContent;
+      const expressao = new RegExp(this.value, "i");
+
+      if (!expressao.test(nome)) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (let i = 0; 0 < pacientes.length; i++) {
+      const paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
+  }
+});
