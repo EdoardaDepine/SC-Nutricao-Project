f0038eb58dba8a10b8001af0d9f3aeb52efb72cd
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-28 15:23:34 -0300

    implementando animação na remoção de um paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,9 @@
 const tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function (event) {
-  event.target.parentNode.remove();
+  event.target.parentNode.classList.add("transicaoExclusao");
+
+  setTimeout(function () {
+    event.target.parentNode.remove();
+  }, 500);
 });
